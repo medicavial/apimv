2726336989aa3f85e3755b20842a48228490287f
Sergio Alcala <checo_2k2@hotmail.com>
2016-09-02 13:16:57 -0500

    se agrego historial web para SACE
    se agrego el modelo que apunta a la tabla de Historial de MySQL y se
    agrego a la clase historial

--- app/classes/Historiales.php
+++ app/classes/Historiales.php
@@ -2,6 +2,29 @@
 
 class Historial {
 
+
+	//parametros iniciales
+	public $titulo;
+	public $descricion;
+	public $folio;
+	public $usuario;
+	public $etapa;
+	public $entrega;
+
+	// funcion para guardar en historial web del SACE
+	public function guardar(){
+
+		$Historial = new HistorialWeb;
+		$Historial->HIS_fecha = date('Y-m-d H:i:s');
+		$Historial->HIS_titulo = $this->titulo;
+		$Historial->HIS_descripcion = $this->descripcion;
+		$Historial->Exp_folio = $this->folio;
+		$Historial->Exp_etapa = $this->etapa;
+		$Historial->Exp_entrega = $this->entrega;
+		$Historial->USU_login = $this->usuario;
+		$Historial->save();
+	}
+
     public static function altaOriginal($folio,$etapa,$entrega)
     {
 
